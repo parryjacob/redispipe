e9d5a8fd2c829793bc9741111da2339367eb5652
Jacob Parry <jacob@jacobparry.ca>
2016-02-02 17:50:13 -0500

    Push back to Python 2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(*paths):
 
 setup(
     name='redispipe',
-    version='1.0.5',
+    version='1.0.6',
     description='Wrap a process\' stdin/stdout/stderr pipes with Redis pubsub queues',
     long_description=read('README.md'),
     url='https://github.com/parryjacob/redispipe',
@@ -19,7 +19,7 @@ setup(
     scripts=['redispipe'],
     include_package_data=False,
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 2 - Alpha',
         'Environment :: Console',
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: MIT License',
